2dee98968f35be898336f4b9ea15cd9824f16fca
Ike Ellsworth <isaac@kina.net>
2021-04-25 13:31:43 -0600

    ???

---
+++ Assets/Scripts/MousePosititionTester.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MousePosititionTester : MonoBehaviour
+{
+    private Vector3 _position;
+    private PlayerInput playerInput;
+
+    private string keyboardId;
+    
+}
