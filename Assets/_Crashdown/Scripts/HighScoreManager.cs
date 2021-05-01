2e6862c4b1ad8c208faeb85b0bb18a4e0f038320
tkeene <eneekmot@yahoo.com>
2021-05-01 16:06:10 -0500

    High Score Screen
    * Added high score screen.
    * High score screen does not yet track secrets.
    * Mashing Interact during a game over will speed up the transition to the high score screen.

--- Assets/_Crashdown/Scripts/HighScoreManager.cs
+++ Assets/_Crashdown/Scripts/HighScoreManager.cs
@@ -7,11 +7,27 @@ using UnityEngine.InputSystem;
 public class HighScoreManager : MonoBehaviour
 {
     public float timeToHoldInteractButton = 1.0f;
+    public TMPro.TextMeshProUGUI highScoreOutput;
+    public string victoryMessage = "CorpsCore.exe not found, system data unrecoverable.";
+    public string gameOverMessage = "Virus Crashdown.exe detcted and quarantined. Game over n00b!";
+    public int rollingOutputSpeedInFrames = 5;
 
     private Controls _controls;
 
     private bool interactIsHeld = false;
     private float currentHeldTime = 0.0f;
+
+    private string originalHighScoreString = "NULL";
+    private int currentRollingOutputCounter;
+    private string formatVictoryOrGameOverMessage = "NULL";
+    private string formatEnemiesKilled = "NULL";
+    private string formatBossesKilled = "FF";
+    private string formatSecretsFound = "FF";
+    private string formatTimeTaken = "FF.FF.FF";
+    private string formatWeaponUsed = "404";
+    private string formatTextColor = "white";
+
+    private bool isDoneWithHighScoreDisplay = false;
 
     private void OnEnable()
     {
@@ -23,6 +39,42 @@ public class HighScoreManager : MonoBehaviour
         _controls.Player.Interact.performed += OnInteractDown;
         _controls.Player.Interact.canceled += OnInteractUp;
         _controls.Player.Interact.Enable();
+
+        originalHighScoreString = highScoreOutput.text;
+        if (CrashdownGameRoot.TotalBossesKilled > 0)
+        {
+            formatVictoryOrGameOverMessage = victoryMessage;
+            formatTextColor = "green";
+        }
+        else
+        {
+            formatVictoryOrGameOverMessage = gameOverMessage;
+            formatTextColor = "#f88";
+        }
+
+        formatEnemiesKilled = CrashdownGameRoot.TotalEnemiesKilled.ToString();
+        while (formatEnemiesKilled.Length < 4)
+        {
+            formatEnemiesKilled = "0" + formatEnemiesKilled;
+        }
+
+        formatBossesKilled = CrashdownGameRoot.TotalBossesKilled.ToString();
+        while (formatBossesKilled.Length < 2)
+        {
+            formatBossesKilled = "0" + formatBossesKilled;
+        }
+
+        Debug.LogError("TODO: Track secrets");
+        //formatSecretsFound = ;
+
+        float timeMinutes = Mathf.Floor(CrashdownGameRoot.TotalTimeUsed / 60.0f);
+        float timeSeconds = Mathf.Repeat(CrashdownGameRoot.TotalTimeUsed, 60.0f);
+        formatTimeTaken = string.Format("{0:00.}.{1:00.00}", timeMinutes, timeSeconds);
+
+        if (CrashdownGameRoot.FinalWeaponUsed != null)
+        {
+            formatWeaponUsed = CrashdownGameRoot.FinalWeaponUsed;
+        }
     }
 
     private void OnDisable()
@@ -45,8 +97,7 @@ public class HighScoreManager : MonoBehaviour
 
     void Update()
     {
-        //if (interactIsHeld)
-        // END OF JAM, NO TIME FOR A HIGH SCORE SCREEN! KICK THEM BACK TO THE TITLE SCREEN AFTER ONE SECOND!
+        if (interactIsHeld)
         {
             if (currentHeldTime > timeToHoldInteractButton)
             {
@@ -56,6 +107,14 @@ public class HighScoreManager : MonoBehaviour
             {
                 currentHeldTime += Time.deltaTime;
             }
+        }
+
+        if (!isDoneWithHighScoreDisplay)
+        {
+            highScoreOutput.text = string.Format(originalHighScoreString, formatVictoryOrGameOverMessage,
+                formatEnemiesKilled, formatSecretsFound, formatBossesKilled, formatTimeTaken, formatWeaponUsed,
+                formatTextColor);
+            isDoneWithHighScoreDisplay = true;
         }
     }
 }
