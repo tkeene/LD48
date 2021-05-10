825c50a40899c07859c5bb5baa648152dcdd6adf
tkeene <eneekmot@yahoo.com>
2021-05-09 20:49:29 -0500

    Colored Bullets
    The player's bullets are now green and enemy bullets are now red.

--- Assets/_Crashdown/Scripts/Projectile.cs
+++ Assets/_Crashdown/Scripts/Projectile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public GameObject Renderer;
+    public GameObject dummyRenderer;
     public SphereCollider reflectionCollider;
 
     public WeaponDefinition MyWeaponData { get; set; }
@@ -13,6 +13,8 @@ public class Projectile : MonoBehaviour
     public float RemainingLifetime { get; set; }
     public int RemainingHits { get; set; }
     public uint MyId { get; set; }
+
+    private CosmeticEffect spawnedEffect = null;
 
     public static List<Projectile> activeProjectiles = new List<Projectile>();
     public static Dictionary<Collider, IGameActor> reflectingColliders = new Dictionary<Collider, IGameActor>();
@@ -29,6 +31,10 @@ public class Projectile : MonoBehaviour
         activeProjectiles.Remove(this);
         actorsHitbyProjectile.Remove(MyId);
         reflectingColliders.Remove(reflectionCollider);
+        if (spawnedEffect != null)
+        {
+            spawnedEffect.Despawn();
+        }
     }
 
     public static void Spawn(Projectile prefab, WeaponDefinition weapon, IGameActor owner, Vector3 position, Quaternion rotation, uint projectileId)
@@ -44,7 +50,15 @@ public class Projectile : MonoBehaviour
         RemainingLifetime = weapon.lifetime;
         RemainingHits = weapon.maximumEnemiesHit;
         MyId = projectileId;
-        Renderer.SetActive(!weapon.hiddenAttack);
+        if (weapon.projectileLockedEffect != null)
+        {
+            spawnedEffect = CosmeticEffect.Spawn(weapon.projectileLockedEffect, weapon.lifetime, transform.position, transform.rotation, this.transform);
+            dummyRenderer.SetActive(false);
+        }
+        else
+        {
+            dummyRenderer.SetActive(!weapon.hiddenAttack);
+        }
         // Don't spawn the actorsHitByProjectile array until it actually hits something.
         if (weapon.reflectsOtherAttacks)
         {
